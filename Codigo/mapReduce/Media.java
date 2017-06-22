6e356bd43ff71661663c7735ecd7c965b01f12c3
caiotavaresc <ctccaio@gmail.com>
2017-06-22 17:38:33 -0300

    Pequena refatoração

--- Codigo/mapReduce/Media.java
+++ Codigo/mapReduce/Media.java
@@ -6,49 +6,19 @@ package mapReduce;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.StringTokenizer;
-
-import mapReduce.util.KeyMaker;
-import mapReduce.util.ValueMaker;
+import mapReduce.mappers.GroupMapper;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
-import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.util.GenericOptionsParser;
 
 public class Media {
-    
-    public static class MediaMapper extends Mapper<Object, Text, Text, DoubleWritable>{
-
-        public void map(Object key, Text value, Context context) throws IOException, InterruptedException 
-        {
-            String formato = context.getConfiguration().get("formato");
-            String variavel = context.getConfiguration().get("variavel");
-            String chave = KeyMaker.keyMaker(formato, value);
-            
-            //Desconsiderar a linha de cabecalho
-            if(((LongWritable) key).get() == new Long(0))
-                return;
-            
-            //Obter o valor da variavel que se esta tirando a media
-            Double valor = ValueMaker.valueMaker(variavel, value);
-            
-            //Se o valor retornado e nulo -> nao foi encontrada aquela variavel naquele dia
-            if(valor==null)
-                return;
-            
-            context.write(new Text(chave), new DoubleWritable(valor));
-        }
-    }
   
     public static class MediaReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable> 
     {
@@ -93,34 +63,34 @@ public class Media {
         */
         
         Configuration conf = new Configuration();
-        /*String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
         
         if (otherArgs.length != 5) 
         {
             System.err.println("Utilizacao: media <diretorio_base_entrada> <elemento> <intervalo_anos> <formato_saida> <diretorio_saida>");
             System.exit(2);
-        }*/
+        }
         
         String variavel, formato, saida;
-        //variavel = otherArgs[1];
-        //formato = otherArgs[3];
-        //saida = otherArgs[4];
+        variavel = otherArgs[1];
+        formato = otherArgs[3];
+        saida = otherArgs[4];
         
         //Remover depois
-        variavel = "TEMP";
-        formato = "Y";
-        saida = "/home/caio/gsod/saida";
+        //variavel = "TEMP";
+        //formato = "Y";
+        //saida = "/home/caio/gsod/saida";
         
         conf.set("variavel", variavel);
         conf.set("formato", formato);
         
         //Selecionar as pastas do intervalo de anos
-        //String diretorioBase = otherArgs[0];
-        //String intervaloDatas = otherArgs[2];
+        String diretorioBase = otherArgs[0];
+        String intervaloDatas = otherArgs[2];
         
         //Remover depois
-        String diretorioBase = "/home/caio/gsod";
-        String intervaloDatas = "2018-2018";
+        //String diretorioBase = "/home/caio/gsod";
+        //String intervaloDatas = "2018-2018";
         
         String[] arrDatas = intervaloDatas.split("-");
         
@@ -135,7 +105,7 @@ public class Media {
         
         Job job = Job.getInstance(conf, "media");
         job.setJarByClass(Media.class);
-        job.setMapperClass(MediaMapper.class);
+        job.setMapperClass(GroupMapper.class);
         job.setCombinerClass(MediaReducer.class);
         job.setReducerClass(MediaReducer.class);
         job.setOutputKeyClass(Text.class);
---
+++ Codigo/mapReduce/mappers/GroupMapper.java
@@ -0,0 +1,37 @@
+/*
+ * Classe responsável por implementar o mapper geneérico
+ * Esse mapper apenas recebe uma linha e busca o formato de agrupamento e a
+ * variável a ser contabilizada e passa para o reducer
+ */
+package mapReduce.mappers;
+
+import java.io.IOException;
+import mapReduce.util.KeyMaker;
+import mapReduce.util.ValueMaker;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class GroupMapper extends Mapper<Object, Text, Text, DoubleWritable>{
+
+    public void map(Object key, Text value, Mapper.Context context) throws IOException, InterruptedException 
+    {
+        String formato = context.getConfiguration().get("formato");
+        String variavel = context.getConfiguration().get("variavel");
+        String chave = KeyMaker.keyMaker(formato, value);
+
+        //Desconsiderar a linha de cabecalho
+        if(((LongWritable) key).get() == new Long(0))
+            return;
+
+        //Obter o valor da variavel que se esta tirando a media
+        Double valor = ValueMaker.valueMaker(variavel, value);
+
+        //Se o valor retornado e nulo -> nao foi encontrada aquela variavel naquele dia
+        if(valor==null)
+            return;
+
+        context.write(new Text(chave), new DoubleWritable(valor));
+    }
+}
