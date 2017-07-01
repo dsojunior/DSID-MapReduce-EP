58e3271a46531be0038e211ba0f87405c61b5582
dsojunior <decio.oliveira.junior@hotmail.com>
2017-07-01 18:48:15 -0300

    plot com reta do MMQ

--- Codigo/mapReduce/runner/GraphicCreator.java
+++ Codigo/mapReduce/runner/GraphicCreator.java
@@ -14,6 +14,9 @@ import org.jfree.data.xy.XYSeriesCollection;
 
 public class GraphicCreator {
    private JFreeChart graphic;
+   public double b;
+   public double a;
+   
  
    public GraphicCreator(String agrupador, String funcao, String results) {
     String chartTitle = "Ep de DSID"; //opcional, passar por parametro
@@ -59,15 +62,66 @@ public class GraphicCreator {
            xValues = xValues + 1.0;
        }
        
+       calculaMMQ (xValues,yValues);
+       
+       //Faz a reta do MMQ
+       series2.add(1.0,previsaoDeValores(1.0));
+       series2.add(1.0,previsaoDeValores(xValues + 1.0));
+       
  
         dataset.addSeries(series1);
-        //dataset.addSeries(series2);
+        dataset.addSeries(series2);
  
         return dataset;
 }
    
     public JPanel getPanel() {
         return new ChartPanel(graphic);
+    }
+    
+    public void calculaMMQ (double ultimoXValue, double [] yValues){
+        //calcular media de X
+        int valorDeX = (int) ultimoXValue;
+        int somaDosValoresDeX = 0;
+        for (int i = valorDeX; i >= 2; i--) {
+            somaDosValoresDeX = somaDosValoresDeX + i;
+        }
+        double mediaDeX = (double) somaDosValoresDeX / (double)valorDeX;     
+        
+        //Calcular media de y
+        double somaDosValoresDeY = 0;
+        for (int j = 0; j < yValues.length; j++) {
+            somaDosValoresDeY = somaDosValoresDeY + yValues[j];
+        }
+        double mediaDeY = somaDosValoresDeY / yValues.length;
+        
+        //Calcular valor de b
+        
+        //Numerador
+        double numerador = 0;
+        double inicioDeX = 2.0;
+        for (int k = 0; k < yValues.length; k++) {
+            numerador = numerador + (inicioDeX*(yValues[k]-mediaDeY));
+            inicioDeX = inicioDeX + 1.0;
+        }
+        
+        //Denominador
+        double denominador = 0;
+        inicioDeX = 2.0;
+        for (int l = 0; l < yValues.length; l++) {
+            denominador = denominador + (inicioDeX*(inicioDeX-mediaDeX));
+            inicioDeX = inicioDeX + 1.0;
+        }
+        
+        b = numerador/denominador;
+        
+        //Calcular valor de A
+        a = mediaDeY - (b*mediaDeX);
+        
+    }
+    
+    public double previsaoDeValores(double xValue){
+        return a + (b*xValue);
     }
     
 //    public static void main(String[] args) {
