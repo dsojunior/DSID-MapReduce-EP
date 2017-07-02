81d4f9abcd4a989e2a9ac987be34134ea92c3a65
Giovani <sousa_giovani@hotmail.com>
2017-07-02 01:08:10 -0300

    Criação do job mmq :cold_sweat:

--- Codigo/mapReduce/MMQ.java
+++ Codigo/mapReduce/MMQ.java
@@ -4,18 +4,23 @@
 package mapReduce;
 
 import java.io.IOException;
-import mapReduce.util.StatisticsJobConf;
-import mapReduce.mappers.MMQMapper;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.GenericOptionsParser;
+import mapReduce.mappers.MMQMapper;
 
 public class MMQ {
      
     public static class MMQReducer extends Reducer<Text,DoubleWritable,Text,DoubleWritable> 
     {
         DoubleWritable result = new DoubleWritable(0.0);
+        @Override
         public void reduce(Text key, Iterable<DoubleWritable> values, Context context) throws IOException, InterruptedException 
         {
             Double soma = 0.0;
@@ -29,9 +34,23 @@ public class MMQ {
     
     public static void main(String[] args) throws Exception 
     {
-        //TODO A ENTRADA DESSE AQUI É OUTRA, COMO CONVERSAMOS, È UM ARQUIVO COM X Y por linha
-        Job job = StatisticsJobConf.getJob(MMQ.class, "mmq", MMQMapper.class, MMQReducer.class, args);
-
+        Configuration conf = new Configuration();
+        String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+        if (otherArgs.length != 3)
+        {
+            System.err.println("Utilizacao: <funcao> <diretorio_base_entrada> <diretorio_saida>");
+            System.exit(2);
+        }
+        String caminho = otherArgs[1];
+        String saida = otherArgs[2];
+        Job job = Job.getInstance(conf, "mmq");
+        job.setJarByClass(MMQ.class);
+        job.setMapperClass(MMQMapper.class);
+        job.setReducerClass(MMQReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(DoubleWritable.class);
+        FileInputFormat.addInputPath(job, new Path(caminho));
+        FileOutputFormat.setOutputPath(job, new Path(saida));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
